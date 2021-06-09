ef5e6baacb46bbf6e829ddb4295091840f79e69d
HenryKihanga <kihangahenry@gmail.com>
2021-06-09 20:03:57 +0300

    Sidebar Complete

--- resources/views/user/details.blade.php
+++ resources/views/user/details.blade.php
@@ -1,4 +1,121 @@
 @extends('layouts.admin')
+@section('navitem')
+<nav class="mt-2 myNavtab">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+        data-accordion="false">
+        <li class="nav-item ">
+            <a href="{{ route('home', Auth::user()->user_id) }}" class="nav-link  " onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    Dashboard
+                </p>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a href="{{ route('addUser') }}" class="nav-link " onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-user-plus"></i>
+                <p>Add User</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('allUsers', Auth::user()->user_id) }}" class="nav-link active "
+                onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-users"></i>
+                <p>Users</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('overallLogs')}}" class="nav-link "
+                onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-clipboard-list"></i>
+                <p>
+                    User Logs
+                </p>
+            </a>
+        </li>
+        @can('manageOrganization')
+            <li class="nav-item">
+                <a href="{{ route('manageOrganization', Auth::user()->user_id) }}" class="nav-link "
+                    onclick="toggle_active_class()">
+                    <i class="nav-icon fas fa-university"></i>
+                    <p>
+                        Organizations
+                    </p>
+                </a>
+            </li>
+        @endcan
+        @can('manageBranch')
+            <li class="nav-item">
+                <a href="{{ route('manageBranch', Auth::user()->user_id) }}" class="nav-link "
+                    onclick="toggle_active_class()">
+                    <i class="nav-icon fas fa-university"></i>
+                    <p>
+                        Branches
+                    </p>
+                </a>
+            </li>
+        @endcan
+        <li class="nav-item">
+            <a href="{{ route('manageDepartment', Auth::user()->user_id) }}" class="nav-link "
+                onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-university"></i>
+                <p>
+                    Departments
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="{{ route('deviceManage', Auth::user()->user_id) }}" class="nav-link "
+                onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-microchip"></i>
+                <p>
+                    Devices
+                </p>
+            </a>
+        </li>
+
+
+        <li class="nav-item">
+            <a href="{{ route('showUserProfile', [Auth::user()->user_id]) }}" class="nav-link "
+                onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-address-card"></i>
+                <p>
+                    View Profile
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-user-edit"></i>
+                <p>
+                    Edit Profile
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('changePassword') }}" class="nav-link " onclick="toggle_active_class()">
+                <i class="nav-icon fas fa-key"></i>
+                <p>
+                    Change Password
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                    Logout
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
+@endsection
 @section('content')
     <div class="content-header">
         <div class="container">
